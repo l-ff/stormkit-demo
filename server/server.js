686a176137269a1f3e92c48b0b6601d4017d3c3e
lf <164806387@qq.com>
2024-08-31 22:33:42 +0800

    init

--- server/server.js
+++
@@ -1,8 +0,0 @@
-import serverless from '@stormkit/serverless'
-
-export const handler = serverless(
-  async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    res.write('Hello from ' + req.url)
-    res.end()
-  }
-)
