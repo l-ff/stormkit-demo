ecf04081a4c398ee47eeafa85e16f21457c54eb9
l-ff <164806387@qq.com>
2024-09-01 01:53:48 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,11 @@
-export const handler = ({ req, res }) => {
-  res.write("hello world");
-  res.end();
+const serverless = require("@stormkit/serverless");
+var express = require('express');
+var app = express();
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+module.exports = {
+  handler: serverless(app),
 };
